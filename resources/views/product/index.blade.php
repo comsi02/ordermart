1d20c520c1c9dc0a44122b90eaebbeec8aa6fedb
comsi02 <comsi02@hotelnow.co.kr>
2016-03-04 00:53:30 +0900

    상품 리스트 수정

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,48 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-
-                <div class="panel-heading">Products</div>
-
-                <div class="panel-body">
-
-                    <!-- body start -->
-                    <a href="{{ url('product/create') }}" class="btn btn-primary pull-right btn-sm">상품추가</a>
-                    <br>
-                    <br>
-                    <div class="table">
-                        <table class="table table-bordered table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th>상품ID</th>
-                                    <th>상품명</th>
-                                    <th>판매가능수량</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($products as $item)
-                                <tr>
-                                    <td>{{ $item->id }}</td>
-                                    <td><a href="{{ url('product/view/'.$item->id) }}">{{ $item->name }}</a></td>
-                                    <td>{{ $item->quantity }}</td>
-                                    <td>
-                                        <a href="{{ url('product/edit/'.$item->id) }}">
-                                            <button type="submit" class="btn btn-primary btn-xs">수정</button>
-                                        </a>
-                                        <a href="{{ url('product/destory/'.$item->id) }}">
-                                            <button type="submit" class="btn btn-danger btn-xs">삭제</button>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        <div class="pagination"> {!! $products->render() !!} </div>
-                    </div>
-                    <!-- body start -->
-
-                </div>
-
+<div class="row">
+  <div class="col-md-12">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">상품</h3>
+        <a href="{{ url('product/create') }}" class="btn btn-primary pull-right btn-sm">상품추가</a>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table class="table table-bordered">
+          <tr>
+            <th>상품ID</th>
+            <th>상품명</th>
+            <th>판매가능수량</th>
+            <th>기능</th>
+          </tr>
+          @foreach($products as $item)
+          <tr>
+            <td>{{ $item->id }}</td>
+            <td>{{ $item->name }}</td>
+            <td>
+              {{ $item->quantity }}
+              <div class="progress progress-xs progress-striped active">
+                <div class="progress-bar progress-bar-danger" style="width: {{$item->quantity}}%"></div>
+              </div>
+            </td>
+            <td>
+              <a href="{{ url('product/edit/'.$item->id) }}">
+                <button type="submit" class="btn btn-primary btn-xs">상세보기</button>
+              </a>
+            </td>
+          </tr>
+          @endforeach
+        </table>
+        <div class="pull-right"> {!! $products->render() !!} </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
