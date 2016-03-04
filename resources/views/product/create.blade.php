96f3baf23814f3b2a3b37eb246ae60aa627156a7
comsi02 <comsi02@hotelnow.co.kr>
2016-03-04 23:43:10 +0900

    디자인 - 상품 추가

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,49 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="panel-heading">Products - Create</div>
-<div class="panel-body">
-
-    <form method="post" action="{{ URL::route('product_create_submit') }}" id="product_create_submit">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-        <div class="form-group">
-            <label for="inputEmail3" class="col-sm-2 control-label">Name</label>
-
-            <div class="col-sm-10">
-                <input type="text" class="form-control" name="name">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="inputPassword3" class="col-sm-2 control-label">Desc</label>
-    
-            <div class="col-sm-10">
-                <input type="text" class="form-control" name="desc">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="inputPassword3" class="col-sm-2 control-label">Quantity</label>
-
-            <div class="col-sm-10">
-                <input type="number" class="form-control" name="quantity">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="inputPassword3" class="col-sm-2 control-label">Salesman</label>
-
-            <div class="col-sm-10">
-                <input type="text" class="form-control" name="salesman" value="{{Auth::user()->name}}" disabled>
-            </div>
-        </div>
-
+<div class="row">
+  <div class="col-md-12">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">상품수정</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+      <!-- /.box-header -->
+      <div class="box-body">
+        <form method="post" action="{{ URL::route('product_create_submit') }}">
+        <table class="table table-bordered">
+          <tr>
+            <th width="20%;">항목</th>
+            <th width="80%;">내용</th>
+          </tr>
+          <tr>
+            <td>상품명</td>
+            <td><input type="text" class="form-control" name="name" placeholder="상품명을 입력해 주세요."></td>
+          </tr>
+          <tr>
+            <td>상품설명</td>
+            <td><textarea class="form-control" rows="10" name="desc" placeholder="상품설명을 입력해 주세요."></textarea></td>
+          </tr>
+          <tr>
+            <td>판매가능수량</td>
+            <td><input type="number" class="form-control" name="quantity" placeholder="0"></td>
+          </tr>
+          <tr>
+            <td>영업사원</td>
+            <td>{{Auth::user()->name}}</td>
+          </tr>
+        </table>
         <div class="box-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">등록</button>
         </div>
-    </form>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
-
 @endsection
