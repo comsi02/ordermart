4a6223c6a3c01298c1b36e69f1a9ae5c1e08b550
comsi02 <comsi02@hotelnow.co.kr>
2016-03-05 16:53:42 +0900

    product table 의 salesman 을 user_id 로 변경

--- database/migrations/2016_02_01_013051_create_products_table.php
+++ database/migrations/2016_02_01_013051_create_products_table.php
@@ -17,12 +17,9 @@ class CreateProductsTable extends Migration
             $table->string('name')->comment('상품명');
             $table->text('desc')->commnet('상품설명');
             $table->integer('quantity')->unsigned()->comment('수량');
-            $table->integer('salesman')->unsigned()->comment('영업사원');
+            $table->integer('user_id')->unsigned()->comment('영업사원');
             $table->string('status')->comment('상태');
             $table->timestamps();
-
-            // foreign key
-            $table->foreign('salesman')->references('id')->on('users');
         });
     }
 
