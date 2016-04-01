d886d0463ea8d0b6ce66304907fe947fd90d1d22
comsi02 <comsi02@hotelnow.co.kr>
2016-04-02 00:01:13 +0900

    작업중 - 상품 사진 넣기

--- resources/views/order/product_view.blade.php
+++ resources/views/order/product_view.blade.php
@@ -17,6 +17,49 @@
             <th width="80%;">내용</th>
           </tr>
           <tr>
+            <td>사진</td>
+            <td>
+              <!-- 사진 넣기 -->
+              <div style="width:400px;height:400px;">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                  </ol>
+
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner" role="listbox" style="align:center">
+                    <div class="item active">
+                      <img src="https://s3.ap-northeast-2.amazonaws.com/ordermart/company/20160401_135915_KzLJ6K4Znk.jpeg" width=400px height=400px alt="...">
+                      <div class="carousel-caption">호텔나우</div>
+                    </div>
+                    <div class="item">
+                      <img src="https://s3.ap-northeast-2.amazonaws.com/ordermart/company/20160401_140354_ydjMILZ1f8.png" width=400px height=400px alt="...">
+                      <div class="carousel-caption">티몬</div>
+                    </div>
+                    <div class="item">
+                      <img src="https://s3.ap-northeast-2.amazonaws.com/ordermart/company/20160401_140052_vYvfD4Hf81.jpg" width=400px height=400px alt="...">
+                      <div class="carousel-caption">네오위즈</div>
+                    </div>
+                  </div>
+
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
             <td>상품ID</td>
             <td>{{$data['product']['id']}}</td>
           </tr>
